dde9f4894d606b22873bae56a79b015881663778
Volodymyr Rudov-Tsymbalist <v.rudovtsymbalist@urancompany.com>
2020-05-31 22:28:27 +0200

    Updates

---
+++ precache-manifest.cb6518bf3011fe49ec99ac467b3038d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57568ba4ce059857cc246fe22c3ac230",
+    "url": "/fabyeon/index.html"
+  },
+  {
+    "revision": "98b56b0da9d48dfc3386",
+    "url": "/fabyeon/static/css/main.3336ac26.chunk.css"
+  },
+  {
+    "revision": "5907b2e59d5276938316",
+    "url": "/fabyeon/static/js/2.c811adaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fabyeon/static/js/2.c811adaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b56b0da9d48dfc3386",
+    "url": "/fabyeon/static/js/main.30ee96df.chunk.js"
+  },
+  {
+    "revision": "448a2399ea2210683cf6",
+    "url": "/fabyeon/static/js/runtime-main.277dfdfd.js"
+  }
+]);
